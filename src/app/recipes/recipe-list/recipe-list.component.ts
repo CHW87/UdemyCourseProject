cca927d4930ad68a9bb40e6bca0833c3c67dc064
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-09-06 16:12:50 +0300

    Allowing the User to add ingredients to the shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,20 +7,26 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes :Recipe[] = [
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
       'This is simply a test',
       'https://myfoodbook.com.au/sites/default/files/styles/car_wd/public/recipe_photo/Whisk_PI_180919_PepPizzaBake_Port_002.jpg'),
-new Recipe(
-      'A test recipe',
+    new Recipe(
+      'Another test recipe',
       'This is simply a test',
       'https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg')
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
