8ecf13bf0cb75ea9b844bf8414f5a73be7552c6e
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-12-08 16:55:17 +0200

    Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError} from "rxjs/operators";
-import {throwError} from "rxjs";
+import {catchError, tap} from "rxjs/operators";
+import {Subject, throwError} from "rxjs";
+import {User} from "./user.model";
 
 export interface AuthResponseData {
   kind: string,
@@ -15,6 +16,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: "root"})
 export class AuthService {
+  user = new Subject<User>()
+
   constructor(private http: HttpClient) {
   }
 
@@ -26,7 +29,14 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       })
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn)
+        }));
   }
 
   login(email: string, password: string) {
@@ -38,24 +48,45 @@ export class AuthService {
         returnSecureToken: true
       }
     )
-    .pipe(catchError(this.handleError))
+      .pipe(catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn)
+        }));
   }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
+    );
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.user.next(user);
+  }
+
   private handleError(errorRes: HttpErrorResponse) {
     let errorMessage = 'An unknown error occurred!'
-            if (!errorRes.error || !errorRes.error.error) {
-              return throwError(errorMessage)
-            }
-            switch (errorRes.error.error.message) {
-              case 'EMAIL_EXISTS':
-                errorMessage = 'This email is already exists';
-                break;
-              case 'EMAIL_NOT_FOUND':
-                errorMessage = 'This email does not exists';
-                break;
-              case 'INVALID_PASSWORD':
-                errorMessage = 'This password is incorrect';
-                break;
-            }
-            return throwError(errorMessage)
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage)
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email is already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exists';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is incorrect';
+        break;
+    }
+    return throwError(errorMessage)
   }
 }
