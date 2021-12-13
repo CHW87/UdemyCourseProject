aa6aec99c917d0ffff93244ea27981b839e90406
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-12-13 16:43:51 +0200

    Adding Auto-Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -60,6 +60,28 @@ export class AuthService {
         }));
   }
 
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirationDate: string;
+    } = JSON.parse(<string>localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     // @ts-ignore
     this.user.next(null);
@@ -70,13 +92,9 @@ export class AuthService {
     const expirationDate = new Date(
       new Date().getTime() + expiresIn * 1000
     );
-    const user = new User(
-      email,
-      userId,
-      token,
-      expirationDate
-    );
+    const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user))
   }
 
   private handleError(errorRes: HttpErrorResponse) {
