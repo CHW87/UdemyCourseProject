97153e25eb0918da7eebe1c64071071c2d425565
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-12-28 16:35:19 +0200

    Understanding Shared Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,21 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {DropdownDirective} from "./shared/dropdown-directive";
 import {ShoppingListService} from "./shoping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {RecipeService} from "./recipes/recipe.service";
 import {AuthComponent} from './auth/auth.component';
-import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shoping-list/shopping-list.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
-  ],
+     ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -34,7 +27,8 @@ import {ShoppingListModule} from "./shoping-list/shopping-list.module";
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService,
     {
@@ -43,10 +37,7 @@ import {ShoppingListModule} from "./shoping-list/shopping-list.module";
       multi: true
     }
   ],
-  bootstrap: [AppComponent],
-  entryComponents:[
-    AlertComponent
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
